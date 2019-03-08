2166b648712d4888bd284c0bc5aecbcd50fbc39a
gearsngenes <shvadaparty@yahoo.com>
2019-03-08 18:07:02 -0500

    WiFi Included

---
+++ EMF_Meter__and_WiFi/Z_Setup_and_Loop.ino
@@ -0,0 +1,47 @@
+Adafruit_ADS1115 ads; //Sets up the Analog to Digital converter
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC); //Sets up the TFT screen
+
+//Declaring the EMF_METER object
+EMF_METER EMF (ads, tft);
+
+double instEMF,minEMF,maxEMF;
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char* ssid = "kvlan2017";
+
+char* password = "covada6164";
+
+char* mqtt_server = "192.168.1.130";
+
+//INSTANTIATE
+gngWiFi WIFI_ONE(espClient,client,ssid,password,mqtt_server);
+
+
+void setup() {
+  Serial.begin(115200);
+  WIFI_ONE.wifiSetup();
+  EMF.ADSsetup();
+  tft.begin();
+  tft.fillScreen(ILI9341_WHITE);
+  tft.fillScreen(ILI9341_BLACK);
+}
+
+void loop() {
+  instEMF=EMF.getEMF(EMF_METER::CHAN_ONE);
+  
+  EMF.runmaxmin();
+  
+  minEMF=EMF.getMin(EMF_METER::CHAN_ONE);
+  maxEMF=EMF.getMax(EMF_METER::CHAN_ONE);
+
+  EMF.makeGrid();
+  EMF.plotData(ILI9341_RED,ILI9341_BLUE); //Graph function with parameters of two colors
+  
+  String DATA = (String)instEMF+","+(String)minEMF+","+(String)maxEMF;
+  //WIFI_ONE.publishDataString(DATA, gngWiFi::D_SET4);
+  WIFI_ONE.publishDataString(DATA, gngWiFi::D_SET3);
+  delay(10);
+}
